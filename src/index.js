f30443e76229abb3cdae5ddd80fd8e4dac3d4c89
Eevan <eevan.redon@gmail.com>
2020-01-26 00:19:04 +0100

    change backend connect URL

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://rails-cooper-api-eevan.herokuapp.com/api/v1";
+axios.defaults.baseURL = "https://cooper-api-eevan.herokuapp.com/";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
